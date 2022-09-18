2c452daa2c04916530c6b57fcdff4b3f4fb383bb
Shravani Sai Ventrapragada <54166478+ssventra@users.noreply.github.com>
2022-09-18 08:37:55 -0700

    Time: 707 ms, Memory: 16.2 MB - LeetHub

---
+++ squares-of-a-sorted-array/squares-of-a-sorted-array.py
@@ -0,0 +1,7 @@
+class Solution:
+    def sortedSquares(self, nums: List[int]) -> List[int]:
+      squares = []
+      for i in nums:
+          squares.append(i*i)
+      return sorted(squares)
+          
